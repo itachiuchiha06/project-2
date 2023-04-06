f493b9e9d0ae0211069f59676bd89e70e10eafeb
itachi uchiha <114676214+itachiuchiha06@users.noreply.github.com>
2023-04-06 11:39:14 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt update
 sudo apt-get full-upgrade -y
-sudo apt-get install python3-pip
-sudo apt-get git
+sudo apt-get install python3-pip -y
+sudo apt-get git -y
 sudo git clone https://github.com/itachiuchiha06/car-prediction.git
 cd car-prediction
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 screen -m -d python3 app.py
